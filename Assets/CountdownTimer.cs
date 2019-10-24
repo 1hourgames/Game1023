b35b2b315c09f63f6b7c0e9a49c5e8baf54f0ae2
Samvid Jhaveri <samvid95@gmail.com>
2019-10-23 23:34:36 -0500

    Single level game. Final build

---
+++ Assets/CountdownTimer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.UIElements;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public float timeLimit;
+    public TextMeshProUGUI timerText;
+
+    public GameObject panel1;
+    public GameObject panel2;
+
+    public GameObject button1;
+    public GameObject button2;
+
+    private string endScene = "Lose";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLimit -= Time.deltaTime;
+
+        if(timeLimit < 20)
+        {
+            panel1.SetActive(false);
+            panel2.SetActive(true);
+            button1.SetActive(true);
+            button2.SetActive(true);
+
+        }
+
+        if(timeLimit < 0)
+        {
+            SceneManager.LoadScene(endScene);
+        }
+        timerText.text = "Time: " + timeLimit.ToString("0.00");
+    }
+}
---
+++ Assets/DecisionScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DecisionScript : MonoBehaviour
+{
+    public int ans;
+    private string winScene = "Win";
+
+    private string endScene = "Lose";
+
+    public void CorrectAns()
+    {
+        SceneManager.LoadScene(winScene);
+    }
+    public void WrongAns()
+    {
+        SceneManager.LoadScene(endScene);
+    }
+
+}
